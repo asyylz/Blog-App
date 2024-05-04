7e71c9ecc75abdb900dc56aabde0c2e9613a0b0b
asyylz <49663592+asyylz@users.noreply.github.com>
2024-05-04 02:18:43 +0100

    update

--- src/components/componentsUI/SearchBar.jsx
+++ src/components/componentsUI/SearchBar.jsx
@@ -1,20 +1,64 @@
+import { useState } from 'react';
+
+const categories = [
+  {
+    id: '663515c3c78ea0a82b9220cb',
+    name: 'World',
+  },
+  {
+    id: '663515c3c78ea0a82b9220dd',
+    name: 'Technology',
+  },
+  {
+    id: '663515c3c78ea0a82b9220ee',
+    name: 'Design',
+  },
+  {
+    id: '663515c3c78ea0a82b9220ff',
+    name: 'Culture',
+  },
+  {
+    id: '663515c3c78ea0a82b922110',
+    name: 'Business',
+  },
+  {
+    id: '663515c3c78ea0a82b922121',
+    name: 'Politics',
+  },
+  {
+    id: '663515c4c78ea0a82b922132',
+    name: 'Science',
+  },
+  {
+    id: '663515c4c78ea0a82b922143',
+    name: 'Health',
+  },
+  {
+    id: '663515c4c78ea0a82b922154',
+    name: 'Style',
+  },
+  {
+    id: '663515c4c78ea0a82b922165',
+    name: 'Travel',
+  },
+];
+
 export default function SearchBar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDropdown = () => setIsOpen(!isOpen);
+  console.log(isOpen);
+
   return (
     <form
       //style={{ border: '2px solid red' }}
       className="max-w-3xl mx-auto py-5"
     >
       <div className="flex">
-        <label
-          for="search-dropdown"
-          className="mb-2 text-sm font-medium text-gray-900 sr-only"
-        >
-          Your Email
-        </label>
         <button
           id="dropdown-button"
-          data-dropdown-toggle="dropdown"
-          className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 "
+          onClick={toggleDropdown}
+          //data-dropdown-toggle="dropdown"
+          className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100"
           type="button"
         >
           All categories
@@ -24,30 +68,38 @@ export default function SearchBar() {
             className="h-4 w-4 ms-2"
           />
         </button>
+
         <div
+          style={{ border: '3px solid red' }}
           id="dropdown"
-          className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44"
+          className={`bg-white divide-y divide-gray-100 rounded-lg shadow w-44 z-10 ${
+            isOpen ? '' : 'hidden'
+          }`}
         >
           <ul
-            className="py-2 text-sm text-gray-700 "
+            className="py-2 text-sm text-gray-700"
             aria-labelledby="dropdown-button"
+            aria-autocomplete="dropdown-button"
           >
-            <li>
-              <button
-                type="button"
-                className="inline-flex w-full px-4 py-2 hover:bg-gray-100 "
-              >
-                Mockups
-              </button>
-            </li>
+            {categories.map((category, index) => (
+              <li key={index}>
+                <button
+                  type="button"
+                  className="inline-flex w-full px-4 py-2 hover:bg-gray-100 "
+                >
+                  {category.name}
+                </button>
+              </li>
+            ))}
           </ul>
         </div>
+
         <div className="relative w-full">
           <input
             type="search"
             id="search-dropdown"
             className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ml-1 focus:ring-2 focus:ring-themeGreenDark focus:border-themeGreenDark focus:outline-none"
-            placeholder=" Takes you to best choice...."
+            placeholder=" To best choice...."
             required
           />
 
