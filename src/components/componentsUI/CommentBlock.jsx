11a412f3a43f12238f85f8df263a17ab15906e1f
asyylz <49663592+asyylz@users.noreply.github.com>
2024-05-06 11:44:35 +0100

    update

--- src/components/componentsUI/CommentBlock.jsx
+++ src/components/componentsUI/CommentBlock.jsx
@@ -28,7 +28,11 @@ export default function CommentBlock({ comments }) {
               className="col-span-12 mb-3 mt-2"
             >
               {openComment ? (
-                <Form method="POST" className="flex flex-col items-end">
+                <Form
+                  onSubmit={() => setOpenComment(false)}
+                  method="POST"
+                  className="flex flex-col items-end"
+                >
                   <textarea
                     id="comment"
                     name="comment"
@@ -63,54 +67,56 @@ export default function CommentBlock({ comments }) {
               )}
             </div>
             <hr className=" my-4" />
-            <div className="p-3 pt-0 md:p-6 md:pt-0">
+            <div
+              //style={{ border: '1px solid red' }}
+              className="p-3 pt-0 md:p-6 md:pt-0"
+            >
               <div className="flex justify-between items-center">
                 <h2 className="text-2xl font-medium">Reader Comments</h2>
               </div>
               <hr className="my-5" />
               {/* <!-- comment item --> */}
-              <div>
-                <div className="flex items-center mb-6">
-                  <div className="w-12 h-12 rounded-full mr-2 overflow-hidden">
-                    <img
-                      src="public/assets/useravatar.svg"
-                      alt=""
-                      className="max-w-full h-auto mx-auto"
-                    />
-                  </div>
-                  <div className="flex flex-grow justify-between">
-                    <div>
-                      <h5 className="font-medium mb-1">
-                        {comments[0].userId.username}
-                      </h5>
-                      {/* <p className="text-sm">
-                        <span className="text-yellow-500">
-                          <i className="fas fa-star"></i>
-                          <i className="fas fa-star"></i>
-                          <i className="fas fa-star"></i>
-                          <i className="fas fa-star"></i>
-                          <i className="fas fa-star"></i>
-                        </span>
-                        <span className="mx-1">5.0</span>
-                      </p> */}
+
+              {comments.map((comment, index) => (
+                <div
+                  key={index}
+                  className={`flex flex-col min-w-full ${
+                    (index + 1) % 2 === 1 ? 'items-start' : 'items-end'
+                  } `}
+                  //style={{ border: '1px solid blue' }}
+                >
+                  <div
+                    //style={{ border: '1px solid red' }}
+                    className="w-full rounded-2xl border border-themeBrown p-5 my-3 max-w-lg"
+                  >
+                    <div
+                      //style={{ border: '1px solid red' }}
+                      className="flex items-center mb-6"
+                    >
+                      <div className="w-12 h-12 rounded-full mr-2 overflow-hidden">
+                        <img
+                          src="public/assets/useravatar.svg"
+                          alt=""
+                          className="max-w-full h-auto mx-auto"
+                        />
+                      </div>
+                      <div className="flex flex-grow justify-between">
+                        <div>
+                          <h5 className="font-medium mb-1">
+                            {comment.userId.username}
+                          </h5>
+                        </div>
+                        <p className="text-sm opacity-50 mb-0">
+                          {comment.createdAt.split('T')[0]}
+                        </p>
+                      </div>
                     </div>
-                    <p className="text-sm opacity-50 mb-0">
-                      {comments[0].createdAt.split('T')[0]}
+                    <p className="text-sm sm:text-[18px] leading-normal opacity-75 mb-6">
+                      {comment.comment}
                     </p>
                   </div>
                 </div>
-                <p className="text-sm sm:text-[18px] leading-normal opacity-75 mb-6">
-                  {comments[0].comment}
-                </p>
-                {/* <div className="flex justify-end">
-                  <button className="hover:text-themeGreenDark hover:bg-gray-200  rounded inline-flex justify-center items-center duration-300 px-3 py-2 mr-6">
-                    <i className="far fa-thumbs-up fs-6 mr-2"></i> Like (20)
-                  </button>
-                  <button className="hover:text-themeGreenDark hover:bg-gray-200  rounded inline-flex justify-center items-center duration-300 px-3 py-2">
-                    <i className="far fa-thumbs-down fs-6 mr-2"></i> Dislike (6)
-                  </button>
-                </div> */}
-              </div>
+              ))}
               <hr className="dark:border-slate-700 my-5" />
             </div>
             <div className="py-6 lg:py-12 text-center">
